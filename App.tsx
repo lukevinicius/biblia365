88b6f71dc94683b70686315c7928d25bd72c8a69
lucas vinicius <lucasviniciusaalves@gmail.com>
2022-01-23 22:54:56 -0300

    add: Font Montserrat

--- App.tsx
+++ App.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 import { Text, StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+import AppLoading from 'expo-app-loading';
 import theme from './src/global/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Text>Open up App.js to start working on your app!</Text>
